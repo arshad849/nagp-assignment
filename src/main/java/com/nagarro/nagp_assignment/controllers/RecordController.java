cbcb346162ef4bf3f408d84c2c464fda85ef7e96
arshad849 <arshad849@gmail.com>
2024-05-29 16:52:15 +0530

    Logs added.

--- src/main/java/com/nagarro/nagp_assignment/controllers/RecordController.java
+++ src/main/java/com/nagarro/nagp_assignment/controllers/RecordController.java
@@ -3,6 +3,7 @@ package com.nagarro.nagp_assignment.controllers;
 import com.nagarro.nagp_assignment.dao.RecordRepository;
 import com.nagarro.nagp_assignment.entity.Record;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,19 +11,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/records")
+@RequestMapping("/records")
 public class RecordController {
   @Autowired
   private RecordRepository recordRepository;
 
+  @GetMapping("/greetings")
+  public String greetings() {
+    log.info("Request Received for greetings");
+    return "Application is up";
+  }
   @GetMapping
   public List<Record> getAllRecords() {
+    log.info("Request Received for getting records");
     return recordRepository.findAll();
   }
 
   @PostMapping
   public Record createRecord(@RequestBody Record record) {
+    log.info("Request Received for creating record "+record);
     return recordRepository.save(record);
   }
 }
--- src/main/java/com/nagarro/nagp_assignment/entity/Record.java
+++ src/main/java/com/nagarro/nagp_assignment/entity/Record.java
@@ -4,14 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 
 @Entity
+@Data
 public class Record {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String data;
-  // getters and setters
 }
 
