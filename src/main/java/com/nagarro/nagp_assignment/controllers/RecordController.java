94d575c6817a2648f83667d428affd5bc9187efe
arshad849 <arshad849@gmail.com>
2024-05-30 16:29:32 +0530

    add function.

--- src/main/java/com/nagarro/nagp_assignment/controllers/RecordController.java
+++ src/main/java/com/nagarro/nagp_assignment/controllers/RecordController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -22,6 +23,21 @@ public class RecordController {
   public String greetings() {
     log.info("Request Received for greetings");
     return "Application is up";
+  }
+
+  @PostMapping("/fibonacci")
+  public Integer fibonacci(@RequestParam int pos) {
+    log.info("Request Received for fibonacci");
+    if (pos <= 1) {
+      return pos;
+    }
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= pos; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   }
   @GetMapping
   public List<Record> getAllRecords() {
